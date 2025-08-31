3bb88adf0f77eefa6d0bcdfbd869570f53c6e7aa
SANTIAGO HERRERA MUNERA <santiagoherrera1141378@correo.itm.edu.co>
2025-08-30 21:44:57 -0500

    Finish Class

---
+++ POO.Concepts.2025/POO.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using POO.Concepts._2025.Core;
+
+try
+{
+    var date1 = new Date(2025, 11, 30);
+    var date2 = new Date(2028, 2, 29);
+
+    Console.WriteLine(date1);
+    Console.WriteLine(date2);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+
+}
+
