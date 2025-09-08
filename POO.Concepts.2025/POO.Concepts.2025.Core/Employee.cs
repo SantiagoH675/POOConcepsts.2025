792ab793f25a26ca65e4089d0060f135bbc827cc
SANTIAGO HERRERA MUNERA <santiagoherrera1141378@correo.itm.edu.co>
2025-09-07 21:28:33 -0500

    Avance

---
+++ POO.Concepts.2025/POO.Concepts.2025.Core/Employee.cs
@@ -0,0 +1,20 @@
+﻿
+namespace POO.Concepts._2025.Core;
+
+public abstract class Employee
+{
+    public int Id { get; set; }
+    public string Firstname { get; set; } = null!;
+    public string Lastname { get; set; } = null!;
+    public bool IsActive { get; set; }
+    public Date BornDate { get; set; } = null!;
+    public Date HireDate { get; set; } = null!;
+
+    public abstract decimal GetValueToPay();
+
+    public override string ToString() =>
+        $"{Id}\t{Firstname} {Lastname}\n\t" +
+        $"Born date.....: {BornDate}\n\t" +
+        $"Hire date.....: {HireDate}";
+}
+
---
+++ POO.Concepts.2025/POO.Concepts.2025.Core/SalaryEmployee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO.Concepts._2025.Core;
+
+public class SalaryEmployee
+{
+    private const decimal MINIMUM_SALARY = 1500000;
+    private decimal _Salary;
+
+    public decimal Salary 
+    { 
+        get => _Salary;
+        set => _Salary = ValidateSalary (value);
+    }
+    private decimal ValidateSalary (decimal salary)
+    {
+        if(salary < MINIMUM_SALARY)
+        {
+            throw new Exception($"The salary: {salary:C2}, is less the minimum ({MINIMUM_SALARY:C2}).");
+        }
+        return salary;
+    }
+}
